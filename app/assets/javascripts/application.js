fde8a65542e282750b99c919d79fd62ed6b628b2
EC2 Default User <ec2-user@ip-172-31-38-113.us-east-2.compute.internal>
2019-12-21 09:25:15 +0000

    before jquary

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require nested_form_fields
+//= jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require nested_form_fields
+
+
+
--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -1,6 +1,6 @@
 class Cooking < ApplicationRecord
    mount_uploader :image, ImageUploader
    belongs_to :user
-   has_many :materials
-   accepts_nested_attributes_for :materials
+   has_many :materials, dependent: :destroy
+   accepts_nested_attributes_for :materials, allow_destroy: true
 end
